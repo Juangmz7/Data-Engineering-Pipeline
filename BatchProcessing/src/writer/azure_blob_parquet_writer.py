4c244cedceec74dcd2a37f0b98969f884590279c
Juangmz7 <juanjuangmo@gmail.com>
2026-03-26 13:57:01 +0100

    Implemented parquetWriter implementation for Azure storage

---
+++ BatchProcessing/src/writer/azure_blob_parquet_writer.py
@@ -0,0 +1,52 @@
+from anyio import Path
+
+from parquet_writer import ParquetWriter
+from util.id_generator import IdGenerator
+from util.pipeline_log_formatter import get_pipeline_logger
+
+from azure.storage.blob import BlobServiceClient
+
+class AzureBlobParquetWriter(ParquetWriter):
+    def __init__(self, connection_string: str, container_name: str, correlation_id: str) -> None:
+        self._correlation_id = correlation_id
+        self._local_id = IdGenerator.generate()
+        self._logger = get_pipeline_logger(
+            class_name=self.__class__.__name__,
+            correlation_id=self._correlation_id,
+            local_id=self._local_id
+        )
+        
+        self._container_name = container_name
+        self._blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        self._logger.info(f"AzureBlobParquetWriter initialized for container: {self._container_name}")
+
+    def write(self, source_path: str, destination: str) -> None:
+        src = Path(source_path)
+        self._logger.info(f"Uploading data from {src} to Azure Blob: {destination}")
+        
+        if not src.exists():
+            raise FileNotFoundError(f"Source file not found: {src}")
+        
+        try:
+            blob_client = self._blob_service_client.get_blob_client(
+                container=self._container_name, 
+                blob=destination
+            )
+            with open(src, "rb") as data:
+                blob_client.upload_blob(data, overwrite=True)
+
+            self._logger.info("Data successfully uploaded to Azure Blob Storage.")
+        
+        except IOError as e:
+            # Handles local disk issues during the read operation
+            self._logger.error(f"Local file system error reading {src}: {e}")
+            raise
+            
+        except AzureError as e:
+            # Handles network, authentication, or service issues from Azure
+            self._logger.error(f"Azure SDK error during upload to {destination}: {e}")
+            raise
+
+        except Exception as e:
+            self._logger.critical(f"Unexpected system or runtime error during Azure upload: {e}", exc_info=True)
+            raise
