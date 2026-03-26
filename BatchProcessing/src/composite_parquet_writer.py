365278ca7517d9173aea39292687d3c1e7dac3aa
Juangmz7 <juanjuangmo@gmail.com>
2026-03-26 13:56:01 +0100

    Implemented class for calling al kind of writers available

---
+++ BatchProcessing/src/composite_parquet_writer.py
@@ -0,0 +1,30 @@
+
+
+import pandas as pd
+
+from util.id_generator import IdGenerator
+from util.pipeline_log_formatter import get_pipeline_logger
+from writer import ParquetWriter
+
+"""
+    ParquetWriter implementation that delegates the write operation to multiple underlying ParquetWriter instances.
+"""
+class CompositeParquetWriter(ParquetWriter):
+    def __init__(self, writers: list[ParquetWriter], correlation_id: str) -> None:
+        self._writers = writers
+        self._correlation_id = correlation_id
+        self._local_id = IdGenerator.generate()
+        self._logger = get_pipeline_logger(
+            class_name=self.__class__.__name__,
+            correlation_id=self._correlation_id,
+            local_id=self._local_id
+        )
+        self._logger.info(f"CompositeParquetWriter initialized with {len(self._writers)} underlying writers.")
+
+    def write(self, source_path: str, destination: str) -> None:
+        self._logger.info(f"Starting composite write operation from {source_path} to destination: {destination}")
+        
+        for writer in self._writers:
+            writer.write(source_path, destination)
+                
+        self._logger.info("Composite write operation completed successfully across all writers.")
