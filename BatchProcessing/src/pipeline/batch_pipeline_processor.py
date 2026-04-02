bf1757e3474837d483fac9ac8d5fee79bf2ae1d8
Juangmz7 <juanjuangmo@gmail.com>
2026-04-02 14:35:05 +0200

    Implemented pipeline_processor classes

---
+++ BatchProcessing/src/pipeline/batch_pipeline_processor.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from typing import List
+
+from shared.contracts.data_writer import DataWriter
+from shared.contracts.pipeline_processor import PipelineProcessor
+from BatchProcessing.src.processor.trip_data_processor import TripDataProcessor
+from implementations.composite_parquet_writer import CompositeWriter
+
+class BatchPipelineProcessor(PipelineProcessor):
+    
+    def run_reader(self, execution_date: str, output_path: str) -> str:
+        # TODO: Instantiate and invoke the Reader contract.
+        return output_path
+
+    def run_validator(self, input_path: str) -> str:
+        # TODO: Instantiate and invoke the initial Validator contract.
+        return input_path
+
+    def run_processor(self, input_path: str, output_path: str, correlation_id: str) -> str:
+        df = pd.read_parquet(input_path)
+        
+        processor = TripDataProcessor(correlation_id=correlation_id)
+        processed_df = processor.process(df)
+        
+        processed_df.to_parquet(output_path, index=False)
+        return output_path
+
+    def run_validator_backup(self, input_path: str) -> str:
+        # TODO: Instantiate and invoke the secondary/backup Validator contract.
+        return input_path
---
+++ shared/contracts/pipeline_processor.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from shared.contracts.data_writer import DataWriter
+from shared.implementations.composite_parquet_writer import CompositeWriter
+
+class PipelineProcessor(ABC):
+    
+    @abstractmethod
+    def run_reader(self, execution_date: str, output_path: str) -> str:
+        pass
+
+    @abstractmethod
+    def run_validator(self, input_path: str) -> str:
+        pass
+
+    @abstractmethod
+    def run_processor(self, input_path: str, output_path: str, correlation_id: str) -> str:
+        pass
+
+    @abstractmethod
+    def run_validator_backup(self, input_path: str) -> str:
+        pass
+
+    def run_writer(self, 
+                   source_path: str, 
+                   final_destination: str, 
+                   correlation_id: str,
+                   writers: List[DataWriter]) -> None:
+        
+        composite_writer = CompositeWriter(
+            writers=writers, 
+            correlation_id=correlation_id
+        )
+        
+        # execution of the composite pattern
+        composite_writer.write(source_path=source_path, destination=final_destination)
