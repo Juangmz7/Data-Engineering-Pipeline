c4175b7f034a80fca0645aefc4ecf6af9c7ef4d8
Juangmz7 <juanjuangmo@gmail.com>
2026-04-02 15:51:05 +0200

    Update pipeline_dag_executions.py

--- shared/implementations/pipeline_dag_executions.py
+++ shared/implementations/pipeline_dag_executions.py
@@ -1,12 +1,11 @@
-from typing import Type, List
+import shutil
+from typing import Type
 
-from airflow.models import Variable
 from airflow.hooks.base import BaseHook
+from anyio import Path
 
-from shared.contracts.data_writer import DataWriter
 from shared.contracts.pipeline_processor import PipelineProcessor
-from BatchProcessing.src.writer.local_parquet_writer import LocalParquetWriter
-from BatchProcessing.src.writer.azure_blob_parquet_writer import AzureBlobParquetWriter
+from shared.contracts.validation_schema import DataFrameSchema
 
 
 def execute_reader(processor_class: Type[PipelineProcessor], execution_date: str, output_path: str) -> str:
@@ -14,16 +13,29 @@ def execute_reader(processor_class: Type[PipelineProcessor], execution_date: str
     return processor.run_reader(execution_date, output_path)
 
 
-def execute_validator(processor_class: Type[PipelineProcessor], input_path: str) -> str:
+def execute_validator(
+    processor_class: Type[PipelineProcessor], 
+    schema_class: Type[DataFrameSchema],
+    input_path: str, 
+    quarantine_path: str, 
+    correlation_id: str
+) -> str:
     processor: PipelineProcessor = processor_class()
-    return processor.run_validator(input_path)
+    
+    try:
+        return processor.run_validator(input_path, schema_class, correlation_id)
+    except ValueError as e:
+        Path(quarantine_path).parent.mkdir(parents=True, exist_ok=True)
+        shutil.move(input_path, quarantine_path)
+        
+        raise AirflowSkipException(
+            f"Validation contract broken. Data safely routed to quarantine: {quarantine_path}"
+        ) from e
 
 
-def execute_processor(processor_class: Type[PipelineProcessor], input_path: str, output_path: str, correlation_id: str) -> str:
+def execute_processor(processor_class: Type[PipelineProcessor],
+                      input_path: str,
+                      output_path: str,
+                      correlation_id: str) -> str:
     processor: PipelineProcessor = processor_class()
     return processor.run_processor(input_path, output_path, correlation_id)
-
-
-def execute_validator_backup(processor_class: Type[PipelineProcessor], input_path: str) -> str:
-    processor: PipelineProcessor = processor_class()
-    return processor.run_validator_backup(input_path)
